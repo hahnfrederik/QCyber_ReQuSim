d3003fca5996087718c72d905bd4ea9c6dbd6ba4
asiangorilla <jan.m.hanes@gmail.com>
2026-05-26 16:10:24 +0200

    beauty changes to import

--- QCtest/verification_rounds.py
+++ QCtest/verification_rounds.py
@@ -4,8 +4,10 @@ import os
 sys.path.insert(0,'../src/requsim/libs')
 
 import numpy as np
-import matrix as mat
-import aux_functions as af
+#import matrix as mat
+import requsim.libs.matrix as mat
+#import aux_functions as af
+import requsim.libs.aux_functions as af
 """ Verification protocol form paper Experimental Quantum Electronic Voting
 """
 
