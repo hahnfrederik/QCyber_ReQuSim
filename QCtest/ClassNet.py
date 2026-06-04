2de676268147171a1637fc4892ebb1723eae9bd8
asiangorilla <jan.m.hanes@gmail.com>
2026-06-04 14:18:45 +0200

    fixed the loops, so the both loops are located in the actor class

--- QCtest/ClassNet.py
+++ QCtest/ClassNet.py
@@ -5,12 +5,12 @@ from multiprocessing import Process, Queue, JoinableQueue
 
 N = 5
 S = 5
-X = [0, 0, 0, 0, 1]
+X = [0, 0, 1, 0, 0]
 
 network = Network(N)
 actors = []
 actor_process = []
-output_queue = JoinableQueue(N)
+output_queue = JoinableQueue()
 for i in range(N):
     actors += [
         Actor(
@@ -22,22 +22,17 @@ for i in range(N):
     ]
 ans = []
 for i in range(N):
-    ordering = np.random.permutation(N)
-    for j, actor in enumerate(actors):
-        curr_a_proc = Process(target=actor.logic_or, args=(N, X[j], ordering, S))
-        curr_a_proc.daemon = True
-        curr_a_proc.start()
-        actor_process += [curr_a_proc]
+    orderings = []
+    orderings += [np.random.permutation(N)]
+for j, actor in enumerate(actors):
+    curr_a_proc = Process(target=actor.logic_or, args=(N, X[j], orderings, S))
+    curr_a_proc.daemon = True
+    curr_a_proc.start()
+    actor_process += [curr_a_proc]
 
-    for ap in actor_process:
-        ap.join()
-    inter_ans = []
-    while not (output_queue.empty()):
-        output = output_queue.get()
-        inter_ans += [output]
-    ans += [inter_ans]
-y_i = 0
-for ans_n in ans:
-    if ans_n[0] == 1:
-        y_i = 1
-print(y_i)
+for ap in actor_process:
+    ap.join()
+while not (output_queue.empty()):
+    output = output_queue.get()
+    ans += [output]
+print(ans)
