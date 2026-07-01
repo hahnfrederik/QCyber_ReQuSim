80c0c3c1d4d30643a75fe65a95731c3abe9b20d8
asiangorilla <jan.m.hanes@gmail.com>
2026-07-01 09:26:45 +0200

    added generalized pauli matrices and rounding feature

--- QCtest/qdit_ext_matrix.py
+++ QCtest/qdit_ext_matrix.py
@@ -15,6 +15,15 @@ def z_d(d, i):
 
 def x_d(d, i):
     state_vec = z_d(d, i)
+    return Ha_d(d) @ state_vec
+
+
+def complex_isclose(a):
+    reals = np.real(a)
+    reals = np.multiply(np.isclose(reals, 0) == False, reals)
+    imags = np.imag(a)
+    imags = np.multiply(np.isclose(imags, 0) == False, imags)
+    return reals + imags * 1j
 
 
 # define the generlaized Hadarmard for qudits, which is the quantum foureier transform
@@ -27,9 +36,28 @@ def Ha_d(d):
     return np.asmatrix(Ha_mat / sqrt(d))
 
 
+# generalized Pauli Z matrix for qudits with optional argument x (as in Wildes book, which is equivalent to multiple applications of the operation)
+def Z_d(d, x=1):
+    diag = np.zeros(d).astype(np.complex128)
+    omeg = np.exp(2 * np.pi * x * 1j / d)
+    for i in range(d):
+        diag[i] = omeg**i
+    return np.diag(diag).astype(np.complex128)
+
+
+# generalized Pauli X matrix, analogous to above
+def X_d(d, x=1):
+    ret = np.zeros((d, d))
+    for i in range(d):
+        ret[(i + x) % d, i] = 1
+    return ret
+
+
+def ghz_d(d, n):
+    state_vecs = [mat.tensor(*([z_d(d, i)] * n)) for i in range(d)]
+    return 1 / sqrt(d) * sum(state_vecs)
+
+
 if __name__ == "__main__":
-    a = z_d(4, 0)
-    print(a)
-    b = Ha_d(4) @ a
-    print(b)
-    print(Ha_d(4).H @ b)
+    a = ghz_d(3, 2)
+    print(a @ mat.H(a))
